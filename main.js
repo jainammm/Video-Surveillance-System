f1bd5a4c97e511c4ca6e9a5bfc7a38e516e68917
jainammm <jainam.iitrpr@gmail.com>
2020-12-09 00:26:50 +0530

    feat: Add basic layout of homepage

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ function createWindow() {
 
     // and load the index.html of the app.
     mainWindow.loadFile('index.html')
+    mainWindow.maximize()
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
